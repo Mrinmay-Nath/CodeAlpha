be1e5fdfc3bcab29d6fac45a449c54f7f24fc937
Mrinmay Nath <87058051+Mrinmay-Nath@users.noreply.github.com>
2024-05-31 20:01:36 +0530

    Delete CodeAlpha_SocialMedia directory

--- CodeAlpha_SocialMedia/middleware.ts
+++
@@ -1,6 +0,0 @@
-export { default } from "next-auth/middleware"
- 
-
-export const config = {
-  matcher: ['/',"/user/:path*","/post/:id*","/profile","/profile/edit","/explore","/messaging/:id*"],
-}
